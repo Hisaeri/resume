efff6294d9c3c604e882bd5a96d201b780b910c0
Fiona <roch.fiona@gmail.com>
2021-01-05 13:52:46 +0900

    create index/styles/includes

---
+++ scripts/includes.js
@@ -0,0 +1,6 @@
+$(function() {
+  $("#skills").load("/components/skills.html");
+  $("#education").load("/components/education.html");
+  $("#experience").load("/components/experience.html");
+  $("#hobbies").load("/components/hobbies.html");
+});
