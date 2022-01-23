dbfd78a9981b86b973aef0a14a22f8c145da5682
Hisaeri <roch.fiona@gmail.com>
2022-01-23 15:05:34 +0900

    Remove hobbies part

--- scripts/includes.js
+++ scripts/includes.js
@@ -3,5 +3,4 @@ $(function() {
   $("#skills").load("/components/skills.html");
   $("#education").load("/components/education.html");
   $("#experience").load("/components/experience.html");
-  $("#hobbies").load("/components/hobbies.html");
 });
