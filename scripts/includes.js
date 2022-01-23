26b5ee997dbac59bb00f475a0f6b2ed22845960d
Hisaeri <roch.fiona@gmail.com>
2022-01-23 15:25:11 +0900

    Re introduce intro component

--- scripts/includes.js
+++ scripts/includes.js
@@ -1,4 +1,5 @@
 $(function() {
+  $("#intro").load("/components/intro.html");
   $("#contact").load("/components/contact.html");
   $("#skills").load("/components/skills.html");
   $("#education").load("/components/education.html");
