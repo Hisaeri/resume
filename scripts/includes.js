e930cb01b03ef6693158780b52fcf4cdd47e573d
Hisaeri <roch.fiona@gmail.com>
2022-01-23 15:23:46 +0900

    Split content into two columns

--- scripts/includes.js
+++ scripts/includes.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#intro").load("/components/intro.html");
+  $("#contact").load("/components/contact.html");
   $("#skills").load("/components/skills.html");
   $("#education").load("/components/education.html");
   $("#experience").load("/components/experience.html");
